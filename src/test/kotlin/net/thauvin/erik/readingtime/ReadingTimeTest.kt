08359a2c63cd34f0f89e9ef3cafcf2209bf882e7
Erik C. Thauvin <erik@thauvin.net>
2020-07-23 10:52:56 -0700

    Added more tests.

--- src/test/kotlin/net/thauvin/erik/readingtime/ReadingTimeTest.kt
+++ src/test/kotlin/net/thauvin/erik/readingtime/ReadingTimeTest.kt
@@ -17,36 +17,54 @@ class ReadingTimeTest {
         assertEquals(3, wordCount("one two three"))
         assertEquals(2, wordCount("    one    two    "))
         assertEquals(7, wordCount(rt.text))
-        assertEquals(210, wordCount(blogPost))
+        assertEquals(505, wordCount(blogPost))
+        assertEquals(391, wordCount(mediumPost))
     }
 
     @Test
     fun testImgCount() {
         assertEquals(1, imgCount(rt.text))
-        assertEquals(4, imgCount(blogPost))
+        assertEquals(11, imgCount(blogPost))
         assertEquals(3, imgCount(mediumPost))
     }
 
     @Test
     fun testReadingTimeInSec() {
         assertEquals((wordCount(rt.text) / (rt.wpm / 60.0)) + 12.0, rt.calcReadingTimeInSec())
+
         rt.text = "<img src=\"#\"> <IMG src=\"#\">"
         assertEquals(12.0 + 11.0, rt.calcReadingTimeInSec())
+
         rt.text = blogPost
-        assertEquals((wordCount(rt.text) / (rt.wpm / 60.0)) + 12.0 + 11.0 + 10.0 + 9.0, rt.calcReadingTimeInSec())
+        assertEquals(
+            (wordCount(rt.text) / (rt.wpm / 60.0)) + 12.0 + 11.0 + 10.0 + 9.0 + 8.0 + 7.0 + 6.0 + 5.0 + 4.0 + 3.0 + 3.0,
+            rt.calcReadingTimeInSec()
+        )
+
+        rt.excludeImages = true
+        assertEquals((wordCount(rt.text) / (rt.wpm / 60.0)), rt.calcReadingTimeInSec())
+        rt.excludeImages = false
+
         rt.text = mediumPost
         rt.wpm = 300
-        assertEquals(wordCount(rt.text) / (rt.wpm / 60.0) + 12.0 + 11.0 + 10.0, rt.calcReadingTimeInSec())
+        assertEquals(wordCount(rt.text) / (300.0 / 60.0) + 12.0 + 11.0 + 10.0, rt.calcReadingTimeInSec())
+        rt.wpm = 275
     }
 
     @Test
     fun testReadingTime() {
         rt.text = blogPost
-        assertEquals("2 min read", rt.calcReadingTime())
+        assertEquals("4 min read", rt.calcReadingTime())
+
         rt.plural = "mins read"
-        assertEquals("2 mins read", rt.calcReadingTime())
+        assertEquals("4 mins read", rt.calcReadingTime())
+
         rt.text = mediumPost
         rt.plural = ""
         assertEquals("2", rt.calcReadingTime())
+
+        rt.text = "This is a test."
+        rt.postfix = ""
+        assertEquals("1", rt.calcReadingTime())
     }
 }
