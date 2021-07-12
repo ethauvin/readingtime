a345330421c37c9f768659f13f180a37ec3cf2ce
Erik C. Thauvin <erik@thauvin.net>
2021-07-11 19:22:21 -0700

    Updated dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     id("com.github.ben-manes.versions") version "0.39.0"
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
+    id("io.gitlab.arturbosch.detekt") version "1.18.0-RC1"
     id("jacoco")
     id("java")
     id("java-library")
     id("maven-publish")
-    id("org.jetbrains.dokka") version "1.4.32"
+    id("org.jetbrains.dokka") version "1.5.0"
     id("org.sonarqube") version "3.3"
     id("signing")
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.20"
 }
 
 description = "Estimated Reading Time for Blog Posts, Articles, etc."
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation(platform(kotlin("bom")))
 
-    implementation("org.jsoup:jsoup:1.13.1")
+    implementation("org.jsoup:jsoup:1.14.1")
 
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
