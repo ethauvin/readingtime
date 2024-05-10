8893b9f3b0c987486ae6415820062539992666c8
Erik C. Thauvin <erik@thauvin.net>
2024-05-10 11:45:40 -0700

    Bumped Kotlin to version 1.9.24

--- src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
+++ src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
@@ -64,7 +64,7 @@ public class ReadingTimeBuild extends Project {
         autoDownloadPurge = true;
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
-        final var kotlin = version(1, 9, 22);
+        final var kotlin = version(1, 9, 24);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
                 .include(dependency("org.jsoup", "jsoup", version(1, 17, 2)));
@@ -140,6 +140,7 @@ public class ReadingTimeBuild extends Project {
     public void jacoco() throws IOException {
         new JacocoReportOperation()
                 .fromProject(this)
+                .sourceFiles(new File(srcMainDirectory(), "kotlin"))
                 .execute();
     }
 
