dbc782aca8dce131d6c4c2af1a87eefb1eea7c58
Erik C. Thauvin <erik@thauvin.net>
2025-03-16 12:03:52 -0700

    Bump JUnit to version 5.12.1

--- src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
+++ src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
@@ -73,8 +73,8 @@ public class ReadingTimeBuild extends Project {
                 .include(dependency("org.jsoup", "jsoup", version(1, 18, 1)));
         scope(test)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-test-junit5", kotlin))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 12, 1)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 12, 1)));
 
         publishOperation()
                 .repository(version.isSnapshot() ? repository(SONATYPE_SNAPSHOTS_LEGACY.location())
