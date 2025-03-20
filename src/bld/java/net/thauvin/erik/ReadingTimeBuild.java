24e6da2305908aaa5f7206e704f5317dd52c1d21
Erik C. Thauvin <erik@thauvin.net>
2025-03-20 09:52:52 -0700

    Add Kotlin compile options for JDK 24

--- src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
+++ src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
@@ -38,6 +38,7 @@ import rife.bld.extension.DokkaOperation;
 import rife.bld.extension.JacocoReportOperation;
 import rife.bld.extension.dokka.LoggingLevel;
 import rife.bld.extension.dokka.OutputFormat;
+import rife.bld.extension.kotlin.CompileOptions;
 import rife.bld.operations.exceptions.ExitStatusException;
 import rife.bld.publish.PomBuilder;
 import rife.bld.publish.PublishDeveloper;
@@ -116,8 +117,11 @@ public class ReadingTimeBuild extends Project {
     @BuildCommand(summary = "Compiles the Kotlin project")
     @Override
     public void compile() throws Exception {
+        final var options = new CompileOptions();
+        options.jvmOptions().add("--enable-native-access=ALL-UNNAMED");
         new CompileKotlinOperation()
                 .fromProject(this)
+                .compileOptions(options)
                 .execute();
     }
 
