384f8f40d940070c01a98408629037122cbb1388
Erik C. Thauvin <erik@thauvin.net>
2026-08-06 12:45:06 -0700

    Bump JSoup to version 1.23.1

--- src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
+++ src/bld/java/net/thauvin/erik/ReadingTimeBuild.java
@@ -77,7 +77,7 @@ public class ReadingTimeBuild extends Project {
         var tomcat = version(11, 0, 24);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
-                .include(dependency("org.jsoup", "jsoup", version(1, 22, 1)));
+                .include(dependency("org.jsoup", "jsoup", version(1, 23, 1)));
         scope(test)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-test-junit5", kotlin))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
